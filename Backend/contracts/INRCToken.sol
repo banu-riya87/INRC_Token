8ad4d7fb1a8272baa208107cc26a6eca6c9940d6
banu-riya87 <98516770+banu-riya87@users.noreply.github.com>
2022-07-19 11:53:20 +0530

    Update INRCToken.sol

--- Backend/contracts/INRCToken.sol
+++ Backend/contracts/INRCToken.sol
@@ -5,10 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-pragma solidity ^0.8.0;
 
 contract INRC is ERC20 ,Ownable {
+
      using SafeMath for uint;
+
     // USDT token
     IERC20 USDCTokenAddress;
        // !% fee for every PEG token mint and burn
@@ -20,11 +21,11 @@ contract INRC is ERC20 ,Ownable {
     
     }
        
-    function mintToken(uint256 amount) public {
+    function mintToken(uint256 USDCAmount) public {
         // Transfer amount USDC tokens from msg.sender to contract
-        USDCTokenAddress.transferFrom(msg.sender, address(this), amount);
+        USDCTokenAddress.transferFrom(msg.sender, address(this), USDCAmount);
        
-        uint PEGToken = amount.mul(10);
+        uint PEGToken = USDCAmount.mul(10);
 
         // Mint tokens to msg.sender
         _mint(msg.sender,PEGToken);
@@ -33,35 +34,38 @@ contract INRC is ERC20 ,Ownable {
     }
 
     
-    function RedeemToken (uint256 amount) public {
+    function RedeemToken (uint256 INRCamount) public {
        
         // Transfer amount USDC tokens from msg.sender to contract
 
         //Calculate the Proportional amount of USDC        
-        uint tempAmount = (amount.mul(10**18)).div(10);
+        uint tempAmount = (INRCamount.mul(10**18)).div(10);
         
         //Calculate Fee
         uint fee = (tempAmount.div(10**2)).div(2);
 
         //Deduct fee from USDC 
-        uint Originaltoken = (tempAmount.sub(fee)).div(10**18);
+        uint USDCtoken = (tempAmount.sub(fee)).div(10**18);
         
         // Send USDC amount tokens to msg.sender
-        USDCTokenAddress.transfer(msg.sender, Originaltoken);
+        USDCTokenAddress.transfer(msg.sender, USDCtoken);
         
         
         // Burn INRC tokens of msg.sender
-        _burn(msg.sender,amount);
+        _burn(msg.sender,INRCamount);
     }
 
-    function withdrawBalance(uint256 amount) 
+    function withdrawBalance(uint256 withdrawAmount) 
     public 
     onlyOwner 
     {
-        uint256 _contractBalance = uint256(address(this).balance);
-        require (_contractBalance >= amount);
-        address payable owner = payable(msg.sender);
-        owner.transfer(amount);
+        //Get the balance of USDC of this contract
+        uint256 _contractBalance = USDCTokenAddress.balanceOf(address(this));
+
+        require (_contractBalance >= withdrawAmount);
+        
+        //Transfer the USDC to msg.sender 
+        USDCTokenAddress.transfer(msg.sender, withdrawAmount);
        
     }
 
